8de56d6d730c4fe6ba7d99b9c383d5110c87a2ad
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-09-08 17:47:34 -0300

    Atribuindo função ao botão Pesquisar

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log(dados[0].descricao);
+let section = document.getElementById('section-resultado');
+let resultado = '';
+const botao = document.getElementById('botao')
+
+botao.addEventListener('click', function() {
+  for (let i = 0; i < dados.length; i++) {
+    resultado += `
+    <div class="atleta">
+      <h2>${dados[i].titulo}</h2>
+      <p>
+        ${dados[i].descricao}
+      </p>
+      <a target="_blank" href="${dados[i].link}">Mais informações!</a>
+    </div>
+  `
+    section.innerHTML = resultado 
+  }
+})
+
