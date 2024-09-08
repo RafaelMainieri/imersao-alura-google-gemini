6304136ff5b2e5fc6a79cca93dfe1f52fb2f8471
Rafael Mainieri <rafasolmainieri@gmail.com>
2024-09-08 20:42:22 -0300

    Verificações

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-let section = document.getElementById('section-resultado');
-let resultado = '';
+let sectionResultado = document.getElementById('section-resultado');
+let sectionPesquisa = document.getElementById('section-pesquisa');
+
 const botao = document.getElementById('botao')
 
+let resultado = '';
+let titulo = '';
+let tags = '';
+
 botao.addEventListener('click', function() {
-  for (let i = 0; i < dados.length; i++) {
-    resultado += `
-    <div class="atleta">
-      <h2>${dados[i].titulo}</h2>
-      <p>
-        ${dados[i].descricao}
-      </p>
-      <a target="_blank" href="${dados[i].link}">Mais informações!</a>
-    </div>
-  `
-    section.innerHTML = resultado 
+  let campoPesquisa = document.getElementById('input-pesquisa');
+  let valorCampoPesquisa = campoPesquisa.value.toLowerCase();
+
+  if (valorCampoPesquisa === '') {
+    alert('Por favor, informe um atleta')
+    sectionResultado.innerHTML = '' 
+    return
   }
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase();
+    tags = dado.tags.toLocaleLowerCase();
+    if (titulo.includes(valorCampoPesquisa) || tags.includes(valorCampoPesquisa)) {
+      resultado = `
+        <div class="atleta">
+          <h2>${dado.titulo}</h2>
+          <p>
+            ${dado.descricao}
+          </p>
+          <a target="_blank" href="${dado.link}">Mais informações!</a>
+        </div>
+      `
+      sectionResultado.innerHTML = resultado 
+      campoPesquisa.value = ''
+      return
+    } else {
+      sectionResultado.innerHTML = '<p>Nada foi encontrado.</p>'    
+    }
+  }
+  campoPesquisa.value = ''
 })
 
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
   {
     titulo: 'Rebeca Andrade',
     descricao: 'Rebeca Andrade, a jovem ginasta brasileira, conquistou o  coração dos brasileiros com suas incríveis performances. Com uma força e agilidade impressionantes, ela se tornou um dos maiores nomes da ginástica artística mundial. Suas medalhas olímpicas e mundiais, além de sua simpatia e carisma, a tornaram um verdadeiro ícone do esporte nacional. Rebeca é um exemplo de superação e dedicação, inspirando jovens atletas a perseguirem seus sonhos.',
-    link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade'
+    link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+    tags: 'Ginástica artística Olimpíadas Medalhista Brasil Salto Solo'
   },
   {
     titulo: 'Gabriel Medina',
     descricao: 'Gabriel Medina, o surfista brasileiro, é conhecido por suas manobras radicais e títulos mundiais. Com um estilo único e uma paixão pelo mar, ele se tornou um dos principais nomes do surf mundial. Sua conquista do título mundial o colocou entre os maiores surfistas de todos os tempos.',
-    link: 'https://pt.wikipedia.org/wiki/Gabriel_Medina'
+    link: 'https://pt.wikipedia.org/wiki/Gabriel_Medina',
+    tags: 'Surfe Campeonato mundial Praia Brasil Onda'
   },
   {
     titulo: 'Neymar Jr.',
     descricao: 'Neymar Jr., o craque do futebol brasileiro, é considerado um dos melhores jogadores do mundo. Com sua habilidade, velocidade e visão de jogo, ele encantou milhões de fãs ao redor do globo. Sua participação em grandes campeonatos e sua passagem por clubes europeus consolidaram seu status como uma das maiores estrelas do futebol.',
-    link: 'https://pt.wikipedia.org/wiki/Neymar'
+    link: 'https://pt.wikipedia.org/wiki/Neymar',
+    tags: 'Futebol Barcelona Brasil Copa do Mundo Atacante Jogador'
   }
 ]
